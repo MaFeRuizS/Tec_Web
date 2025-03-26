218d13992bc073070e0a8cd67045f5c8ca3feb76
Mafer <mariferruizsantiago@gmail.com>
2025-03-25 19:43:44 -0600

    Añadimos la instancia base de Product/Comentamos el contenido anterior se search

---
+++ Actividades/a07/product_app/backend/product-search.php
@@ -0,0 +1,32 @@
+<?php
+    /*include_once __DIR__.'/database.php';
+
+    $search = $_GET['search'];
+
+    if(!empty($search)){
+        $query = "SELECT * FROM productos Where (id = '{$search}' OR nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%') AND eliminado = 0";
+        $result = mysqli_query($conexion, $query);
+        if(!$result){
+            die('Query Error'.mysqli_error($conexion));
+        }
+
+        $json = array();
+        while($row = mysqli_fetch_array($result)){
+            $json[] = array(
+                'name' => $row['nombre'],
+                'description' => $row['detalles'],
+                'id' => $row['id']
+            );
+        }
+        $jsonstring = json_encode($json);
+        echo $jsonstring;}*/
+
+        use TECWEB\MYAPI\Products as Products;
+        require_once __DIR__ . '/myapi/products.php';
+    
+        $prodObj = new Products('marketzone');
+        $prodObj->search();
+    
+        echo $prodObj->getData();
+    
+?>
